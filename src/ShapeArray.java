a982dfeaee50d1dfd51e82009dfbff8c624ed163
Brian Gunther <brian.gunther@outlook.com>
2024-09-06 20:06:57 -0700

    c1

--- src/ShapeArray.java
+++ src/ShapeArray.java
@@ -1,3 +1,13 @@
+/*
+ * 
+ * Module 4 CTA Option 1: Creating an Abstract Class Shape
+ * Brian Gunther
+ * CSC372: Programming II
+ * Colorado State University Global
+ * Dr. Vanessa Cooper
+ * September 8, 2024
+ * 
+ */
 
 public class ShapeArray {
 
